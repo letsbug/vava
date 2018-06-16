bd9502492c343d098e52c818b61525b1d835a80b
ImBugMaker <xman724939995@gmail.com>
2018-06-17 03:09:04 +0800

    Init the HTTP request base class

---
+++ src/api/request.js
@@ -0,0 +1,34 @@
+/**
+ * request.
+ * @author: gzb
+ * @date: 2018-06-17 02:58
+ */
+
+import axios from 'axios'
+import store from '@/store'
+
+import { getToken } from '@/utilities/Token'
+import { Message } from 'element-ui'
+
+const service = axios.create({ baseURL: process.env.BASE_API, timeout: 6000 })
+
+// request interceptor
+service.interceptors.request.use(
+  config => {
+    // Let all request carry token, ['X-Token'] is custom key.
+    if (store.getters.user.token) service.defaults.headers['X-Token'] = getToken()
+    return config
+  }, error => Promise.reject(error)
+)
+
+// response interceptor
+service.interceptors.response.use(
+  response => response,
+  error => {
+    console.log('response' + error) // for debug
+    Message({ message: error.message, type: 'error', duration: 5 * 1000 })
+    return Promise.reject(error)
+  }
+)
+
+export default service
