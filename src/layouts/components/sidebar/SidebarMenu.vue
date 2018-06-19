d31d6aac971c02a19a46d916c718af2f2c7bdaf2
ImBugMaker <xman724939995@gmail.com>
2018-06-19 17:38:23 +0800

    Dynamic 'SidebarMenu' component.

--- src/layouts/components/sidebar/SidebarMenu.vue
+++ src/layouts/components/sidebar/SidebarMenu.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="sidebar-menu">
-    <router-link class="menu-item" to="/dashboard">
-      <svg class="icon-svg" aria-hidden="true"><use xlink:href="#if-home"></use></svg>
-      <cite>Dashboard</cite>
-    </router-link>
+    <template v-for="(route, index) in routes" v-if="!route.hidden">
+      <router-link class="menu-item" v-if="route.children.length <= 1" :to="route.path + '/' + route.children[0].path">
+        <svg class="icon-svg" aria-hidden="true"><use :xlink:href="'#if-' + route.children[0].meta.icon"></use></svg>
+        <cite>{{ route.children[0].meta.title }}</cite>
+      </router-link>
+      <router-link class="menu-item" v-else to="/dashboard">
+        <svg class="icon-svg" aria-hidden="true"><use xlink:href="#if-home"></use></svg>
+        <cite>Dashboard</cite>
+      </router-link>
+    </template>
   </div>
 </template>
 
--- src/router/static.js
+++ src/router/static.js
@@ -3,6 +3,8 @@ import Layout from '@/layouts/Layout'
 /**
  * noCache: Marks whether the route is allowed to be cached so that the DOM is repeatedly rendered when routing is frequently switched.
  *          true - not allow
+ * hidden:  Marks whether the route is visible and displayed when the template is rendered.
+ *          true - hide
  * @type {Route[]}
  */
 export const constantRouterMap = [
@@ -13,10 +15,20 @@ export const constantRouterMap = [
     component: Layout,
     redirect: '/dashboard',
     children: [{
-      path: '/dashboard',
+      path: 'dashboard',
       name: 'dashboard',
       component: () => import('@/views/dashboard/Dashboard'),
-      meta: { title: 'dashboard', nocache: true }
+      meta: { title: 'Dashboard', icon: 'home', nocache: true }
+    }]
+  },
+  {
+    path: '',
+    component: Layout,
+    children: [{
+      path: 'guide',
+      name: 'guide',
+      component: () => null,
+      meta: { title: 'Guide', icon: 'home', nocache: true }
     }]
   }
 ]
