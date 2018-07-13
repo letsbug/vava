31a2d773d62b52a0b57031c9db0f369be6668e23
ImBugMaker <xman724939995@gmail.com>
2018-07-13 20:45:11 +0800

    Add home and back link in 404 page.

--- src/views/errorPages/error404.vue
+++ src/views/errorPages/error404.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="app-errs-with404">
+    <div class="error-logo">
+      <img :src="img" alt="">
+    </div>
     <div class="options">
       <div class="title">Oops! 404!!!</div>
       <div class="subtitle">页面调皮了，不知道跑哪儿去了......</div>
-    </div>
-    <div class="error-logo">
-      <img :src="img" alt="">
+      <div>
+        <el-button type="text" @click.prevent="$router.push('/')">HOME</el-button>
+        <el-button type="text" @click.prevent="$router.go(-1)">BACK</el-button>
+      </div>
     </div>
   </div>
 </template>
