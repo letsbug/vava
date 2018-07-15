9dece9643a6f85f7a8a7cd0b974be88367da33ca
ImBugMaker <xman724939995@gmail.com>
2018-07-15 08:58:54 +0800

    Optimize stylesheet.

--- src/views/errorPages/error404.vue
+++ src/views/errorPages/error404.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="app-errs-with404">
-    <div class="error-logo">
-      <img :src="img" alt="">
-    </div>
-    <div class="options">
-      <div class="title">Oops! 404!!!</div>
-      <div class="subtitle">页面调皮了，不知道跑哪儿去了......</div>
-      <div>
-        <el-button type="text" @click.prevent="$router.push('/')">HOME</el-button>
-        <el-button type="text" @click.prevent="$router.go(-1)">BACK</el-button>
+  <div class="app-errs-container">
+    <div class="app-errs-with404">
+      <div class="error-logo">
+        <img :src="img" alt="">
+      </div>
+      <div class="options">
+        <div class="title">Oops! 404!!!</div>
+        <div class="subtitle">页面调皮了，不知道跑哪儿去了......</div>
+        <div>
+          <el-button type="text" @click.prevent="$router.push('/')">HOME</el-button>
+          <el-button type="text" @click.prevent="$router.go(-1)">BACK</el-button>
+        </div>
       </div>
     </div>
   </div>
