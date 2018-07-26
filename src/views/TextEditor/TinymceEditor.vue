fa4bf02c27a6a321c8dd3b777a74edce6d1a04d8
ImBugMaker <xman724939995@gmail.com>
2018-07-26 14:24:49 +0800

    Tinymce editor test document image localization.

--- src/views/TextEditor/TinymceEditor.vue
+++ src/views/TextEditor/TinymceEditor.vue
@@ -13,7 +13,7 @@ export default {
   data() {
     return {
       content: `
-        <p><img style="float: right;" src="https://www.tiny.cloud/images/glyph-tinymce@2x.png" alt="TinyMCE" height="150px" data-mce-src="images/glyph-tinymce@2x.png" data-mce-style="float: right;"></p>
+        <p><img style="float: right;" src="./static/tinymce/images/glyph-tinymce@2x.png" alt="TinyMCE" height="150px" data-mce-src="images/glyph-tinymce@2x.png" data-mce-style="float: right;"></p>
         <h2>The world’s first rich text editor in the cloud</h2>
         <p>Have you heard about Tiny Cloud? It’s the first step in our journey to help you deliver great content creation experiences, no matter your level of expertise. 50,000 developers already agree. They get free access to our global CDN, image proxy services and auto updates to the TinyMCE editor. They’re also ready for some exciting updates coming soon.</p>
         <p>One of these enhancements is <strong>Tiny Drive</strong>: imagine file management for TinyMCE, in the cloud, made super easy. Learn more at <a href="https://www.tinymce.com/tinydrive/" data-mce-href="https://www.tinymce.com/tinydrive/" data-mce-linkchecker-status="valid">tinymce.com/tinydrive</a>, where you’ll find a working demo and an opportunity to provide feedback to the product team.</p>
