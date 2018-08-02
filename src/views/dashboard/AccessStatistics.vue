7fea576b06b18f7e2865d1b5526bf75a1a0c6a6b
ImBugMaker <xman724939995@gmail.com>
2018-08-02 22:52:32 +0800

    some commits.

--- src/views/dashboard/AccessStatistics.vue
+++ src/views/dashboard/AccessStatistics.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="container-body no-bg dashboard">
     <el-row :gutter="15">
-      <el-col :span="6"><count-panel ref="chart_total_pv" :x-axis="xAxis" :y-axis="yAxis" :datas="datas"></count-panel></el-col>
-      <el-col :span="6"><count-panel ref="chart_total_uv" :x-axis="xAxis" :y-axis="yAxis" :datas="datas"></count-panel></el-col>
-      <el-col :span="6"><count-panel ref="chart_total_ip" :x-axis="xAxis" :y-axis="yAxis" :datas="datas"></count-panel></el-col>
-      <el-col :span="6"><count-panel ref="chart_total_out" :x-axis="xAxis" :y-axis="yAxis" :datas="datas"></count-panel></el-col>
+      <el-col :span="6"><count-panel ref="chart_total_pv" :id="'pv'" :x-axis="xAxis" :y-axis="yAxis" :datas="datas"/></el-col>
+      <el-col :span="6"><count-panel ref="chart_total_uv" :id="'uv'" :x-axis="xAxis" :y-axis="yAxis" :datas="datas"/></el-col>
+      <el-col :span="6"><count-panel ref="chart_total_ip" :id="'ip'" :x-axis="xAxis" :y-axis="yAxis" :datas="datas"/></el-col>
+      <el-col :span="6"><count-panel ref="chart_total_out" :id="'out'" :x-axis="xAxis" :y-axis="yAxis" :datas="datas"/></el-col>
     </el-row>
   </div>
 </template>
@@ -17,9 +17,12 @@ export default {
   components: { CountPanel },
   data() {
     return {
-      xAxis: {},
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      },
       yAxis: {},
-      datas: []
+      datas: [820, 932, 901, 934, 1290, 1330, 1320]
     }
   },
   mounted() {},
--- src/views/dashboard/commons/CountPanel.vue
+++ src/views/dashboard/commons/CountPanel.vue
@@ -2,7 +2,7 @@
   <div class="app-panel count-panel">
     <p class="title">total visits</p>
     <h2 class="text-center counter" :class="(today-yestoday>0)?'text-primary':'text-red'">4987</h2>
-    <div class="chart" id="totalChart">chart viewer</div>
+    <div class="chart" :id="id"></div>
   </div>
 </template>
 
@@ -13,26 +13,44 @@ import 'echarts/theme/infographic'
 export default {
   name: "CountPanel",
   props: {
-    ctype: { type: String, default: 'bar', required: false },
+    id: { type: String, default: 'totalChart' },
+    sname: { type: String, default: '', required: false },
+    stype: { type: String, default: 'line', required: false },
     xAxis: { type: Object, default: {}, required: true },
     yAxis: { type: Object, default: {}, required: true },
     datas: { type: Array, default: () => [], required: false }
   },
   data() { return {
-    series: [{
-      type: this.ctype,
-      data: this.datas,
-    }],
+    chart: null,
+    tooltip: {},
     today: 99,
-    yestoday: 90,
-    option: {
-      title: '',
-      tooltip: { trigger: 'axis', axisPointer: { type: 'cross', label: { backgroundColor: '#6a7985' } } },
-      xAxis: [],
-      yAxis: [],
-      series: []
+    yestoday: 90
+  } },
+  mounted() {
+    this.init()
+  },
+  methods: {
+    init() {
+      this.chart = echarts.init(document.getElementById(this.id), 'infographic')
+      const option = {
+        title: null,
+        yAxis: this.yAxis,
+        xAxis: this.xAxis,
+        series: [{
+          name: this.sname,
+          type: this.stype,
+          data: this.datas
+        }]
+      }
+      this.chart.setOption(option)
     }
-  } }
+  },
+  beforeDestroy() {
+    if (this.chart) {
+      this.chart.dispose()
+      this.chart = null
+    }
+  }
 }
 </script>
 
@@ -43,6 +61,7 @@ export default {
 
     .title {
       margin-top: 0;
+      margin-bottom: $spacer-y * 2;
     }
 
     .counter {
@@ -53,9 +72,8 @@ export default {
     }
 
     .chart {
-      width: 80px;
-      height: 40px;
-      background-color: red;
+      width: 100%;
+      height: 160px;
       margin: 0 auto;
     }
   }
