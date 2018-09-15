3f49b750219b0f5bf1072d9ba277464431136a6d
letsbug <xman724939995@gmail.com>
2018-09-16 00:20:40 +0800

    Reversion...

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"',
+  NODE_ENV: '"prod"',
   BASE_API: '"https://prods.api.com"',
   TINYMCE_RES: '"https://gzb219.github.io/Vue-admin-frame/"'
 }
--- config/test.env.js
+++ config/test.env.js
@@ -3,6 +3,6 @@ const merge = require('webpack-merge')
 const devEnv = require('./dev.env')
 
 module.exports = merge(devEnv, {
-  NODE_ENV: '"testing"',
+  NODE_ENV: '"test"',
   BASE_API: '"https://test.api.com"'
 })
