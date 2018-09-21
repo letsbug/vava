85e49a5f4aeec0e2f9b3c75056797514268a0680
letsbug <xman724939995@Gmail.com>
2018-09-21 10:44:42 +0800

    Add some icons about tables.

--- src/router/modules/tables.js
+++ src/router/modules/tables.js
@@ -13,31 +13,31 @@ const tables = {
     {
       path: 'basic',
       name: 'Basic',
-      meta: { title: 'basic use', icon: '' },
+      meta: { title: 'basic use', icon: 'table-default' },
       component: () => import('@/modules/tables/BasicUse')
     },
     {
       path: 'dynamic',
       name: 'Dynamic',
-      meta: { title: 'dynamic', icon: '' },
+      meta: { title: 'dynamic', icon: 'dynamic' },
       component: () => import('@/modules/tables/Dynamic')
     },
     {
       path: 'editable',
       name: 'Editable',
-      meta: { title: 'editable', icon: '' },
+      meta: { title: 'editable', icon: 'edit' },
       component: () => import('@/modules/tables/Editable')
     },
     {
       path: 'sortable',
       name: 'Sortable',
-      meta: { title: 'sortable', icon: '' },
+      meta: { title: 'sortable', icon: 'sort' },
       component: () => import('@/modules/tables/Sortable')
     },
     {
       path: 'draggable',
       name: 'Draggable',
-      meta: { title: 'draggable', icon: '' },
+      meta: { title: 'draggable', icon: 'drag' },
       component: () => import('@/modules/tables/Draggable')
     }
   ]
