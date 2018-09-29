1abb6b3eb34f937bbf4e546e9ce63bdafd5bd2f4
letsbug <xman724939995@gmail.com>
2018-09-29 12:34:12 +0800

    Optimize interaction effect.

--- src/modules/icons/IconViewer.vue
+++ src/modules/icons/IconViewer.vue
@@ -2,10 +2,9 @@
   <div class="va-body-container icon-viewer">
     <h4 class="va-body-title">SVG Icons&nbsp;&nbsp;<small class="text-muted">Sets of ICONS used by the system itself.</small></h4>
     <blockquote>
-      <p>You can use the built-in SVG icons by using the <code>va-icon</code> component.</p>
-      <p>Click them to copy the code.</p>
+      <p>You can use the built-in SVG icons by using the <code>va-icon</code> component. Click them to copy the code.</p>
     </blockquote>
-    <el-row :gutter="20">
+    <el-row>
       <el-col :xl="3" :lg="4" :md="6" :sm="6" :xs="8" v-for="icon of icons" :key="icon">
         <div class="grid-content" @click="handleClipboard(generateIconCode(icon), $event)">
           <el-tooltip placement="top">
@@ -51,12 +50,23 @@ export default {
     font-size: $font-size-h1;
     text-align: center;
     padding: $spacer-lg 0;
+    margin: 0 $spacer-base;
+    border: solid $border-default-width transparent;
+    border-radius: $radius-base;
     cursor: pointer;
+
+    transition: $transition-border, $transition-transform;
+
+    &:hover {
+      border-color: $color-theme;
+      transform: translate3d(0, -3px, 0);
+      box-shadow: 0 $shadow-lg-offset $shadow-lg-distance $shadow-lg-size rgba($color-theme, 0.2);
+    }
 
     > span {
       display: block;
       margin-top: $spacer-sm;
-      font-size: $font-size-h5;
+      font-size: $font-size-h6;
     }
   }
 }
