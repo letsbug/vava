a99d07d7f3836ab2f43b815584b1752b3e9feef6
letsbug <xman724939995@Gmail.com>
2018-09-29 10:35:17 +0800

    Coding icon viewer component.

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import VaIcon from '@/components/va-icons'
 
+// Register as a global component
 Vue.component('va-icon', VaIcon)
 
-const requires = require.context('./', false, /\.svg$/)
-const reqAll = context => context.keys().map(context)
-reqAll(requires)
+const icons = require.context('./', false, /\.svg$/)
+const req = context => context.keys().map(context)
+req(icons)
