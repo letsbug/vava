1387382897763b34b38134c459e802913bec475f
letsbug <xman724939995@gmail.com>
2018-11-03 11:16:25 +0800

    Add github corner.

--- src/modules/homepage/Homepage.vue
+++ src/modules/homepage/Homepage.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="va-body-container">
+    <github-corner></github-corner>
     <h1>Homepage</h1>
   </div>
 </template>
 
 <script>
+import GithubCorner from '@/components/GithubCorner'
 export default {
-  name: 'Homepage'
+  name: 'Homepage',
+  components: { GithubCorner }
 }
 </script>
