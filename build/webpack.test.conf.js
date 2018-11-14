1e5c103fcd9ea412905582a471ce01871a8fbcf5
letsbug <xman724939995@gmail.com>
2018-11-14 20:15:11 +0800

    config webpack testing env.

--- build/webpack.test.conf.js
+++ build/webpack.test.conf.js
@@ -7,7 +7,7 @@ const merge = require('webpack-merge')
 const baseWebpackConfig = require('./webpack.base.conf')
 
 const webpackConfig = merge(baseWebpackConfig, {
-  mode: 'testing',
+  mode: 'none',
   // use inline sourcemap for karma-sourcemap-loader
   module: {
     rules: utils.styleLoaders()
