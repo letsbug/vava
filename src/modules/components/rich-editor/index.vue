c4cc4ca90485d3b83e4cdc9c88c7fcc563233918
letsbug <xman724939995@gmail.com>
2018-12-19 16:00:55 +0800

    The choice of rich text plug-ins continues to be a struggle...

--- src/modules/components/rich-editor/index.vue
+++ src/modules/components/rich-editor/index.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="va-body-container">
-    <ckeditor :editor="editor" />
+    <!--<ckeditor :editor="editor" />-->
+    CKEditor，TinyMCE，Quill，medium-editor，slatejs，选肥的选瘦的。。。富文本就是个天坑。。。
   </div>
 </template>
 
 <script>
-import CKEditor from '@ckeditor/ckeditor5-vue/dist/ckeditor'
-import Classic from '@ckeditor/ckeditor5-build-classic/build/ckeditor'
+// import CKEditor from '@ckeditor/ckeditor5-vue/dist/ckeditor'
+// import Classic from '@ckeditor/ckeditor5-build-classic/build/ckeditor'
 
 export default {
   name: 'RichEditor',
@@ -14,11 +15,11 @@ export default {
     title: 'Rich editor demo'
   },
   components: {
-    ckeditor: CKEditor.component
+    // ckeditor: CKEditor.component
   },
   data() {
     return {
-      editor: Classic
+      // editor: Classic
     }
   },
   mounted() {
