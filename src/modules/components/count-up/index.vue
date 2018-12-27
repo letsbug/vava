e9c1b71e7f2fc9561f28e87c6020a343fa73cfcf
letsbug <xman724939995@gmail.com>
2018-12-27 16:07:26 +0800

    Optimize some svgs

---
+++ src/modules/components/count-up/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="va-body-container">
+    index
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CountUp',
+  metaInfo: {
+    title: 'CountUp'
+  },
+  data() {
+    return {}
+  }
+}
+</script>
--- src/router/modules/components.js
+++ src/router/modules/components.js
@@ -9,13 +9,24 @@ export default {
     icon: 'thing-component'
   },
   alwaysShow: true,
-  children: [{
-    path: 'rich-editor',
-    name: 'RichEditor',
-    meta: {
-      title: 'richEditor',
-      icon: 'thing-editor-ckeditor'
+  children: [
+    {
+      path: 'rich-editor',
+      name: 'RichEditor',
+      meta: {
+        title: 'richText',
+        icon: 'thing-editor-tinymce'
+      },
+      component: () => import('@/modules/components/rich-editor')
     },
-    component: () => import('@/modules/components/rich-editor')
-  }]
+    {
+      path: 'count-up',
+      name: 'CountUp',
+      meta: {
+        title: 'countUp',
+        icon: 'action-count'
+      },
+      component: () => import('@/modules/components/count-up')
+    }
+  ]
 }
