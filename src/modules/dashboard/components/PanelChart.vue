937cadc8dd180dda5abbafcdcfcc1ad44304cd63
letsbug <xman724939995@gmail.com>
2019-01-18 17:21:23 +0800

    Coding dashboard...

--- src/modules/dashboard/components/PanelChart.vue
+++ src/modules/dashboard/components/PanelChart.vue
@@ -5,6 +5,7 @@
 <script>
 import echarts from 'echarts'
 require('echarts/theme/shine')
+import { tooltip } from '../charts/options'
 
 export default {
   name: 'PanelChart',
@@ -67,25 +68,20 @@ export default {
     init() {
       this.chart = echarts.init(this.$el, 'shine')
 
+      const _tooltip = {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'none'
+        },
+        position: (pt, params, dom, rect, size) =>
+          ((pt[0] + size.contentSize[0]) < this.width)
+            ? [pt[0] + 6, 9]
+            : [pt[0] - size.contentSize[0] - 6, 9]
+      }
+
       this.chart.setOption({
         color: [this.themeColor],
-        tooltip: {
-          trigger: 'axis',
-          axisPointer: {
-            type: 'none'
-          },
-          position: (pt, params, dom, rect, size) =>
-            ((pt[0] + size.contentSize[0]) < this.width)
-              ? [pt[0], 2]
-              : [pt[0] - size.contentSize[0], 2],
-          textStyle: {
-            color: '#343a40',
-            fontSize: 12
-          },
-          padding: [3, 8],
-          extraCssText: 'background-color: rgba(255, 255, 255, .86);' +
-            'box-shadow: 0 0px 2px rgba(0, 0, 0, .25);'
-        },
+        tooltip: Object.assign({}, _tooltip, tooltip),
         grid: {
           top: 16,
           right: 7,
